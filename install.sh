89f6d0234dd80843383508ac1f265947a993c118
Marc Proux <marc.proux@proux.onl>
2024-01-21 23:23:53 +0100

    fix missing file with ipmitool on bookworm

--- install.sh
+++ install.sh
@@ -16,6 +16,11 @@ echo "*** Installing packaged dependencies..."
 if [ -x "$( command -v apt-get )" ]; then
 	apt-get update
 	apt-get install -y build-essential python3-virtualenv python3-dev libsensors4-dev ipmitool
+
+	# Bookworm issue!
+	if [[ ! -f "/usr/share/misc/enterprise-numbers.txt" ]]; then
+		wget -O "/usr/share/misc/enterprise-numbers.txt" "https://www.iana.org/assignments/enterprise-numbers.txt"
+	fi
 elif [ -x "$( command -v dnf )" ]; then
 	dnf groupinstall -y "Development Tools"
 	dnf install -y python3-virtualenv python3-devel lm_sensors-devel ipmitool
